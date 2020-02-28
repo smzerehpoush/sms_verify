0a0fa99b8fd186501cdbb1234e4f5fbc68177f16
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-02-28 17:40:51 +0330

    add basic functionality

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import requests
+import config
+   
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return "Hello World!"
+	return "Hello World!"
+
+@app.route("/v1/process")
+def process():
+	data = {"code" : 0, "message":"processed"}
+	return jsonify(data),200
+def send_sms():
+	pass
+
+def check_serial():
+	pass
+
+if __name__ == "__main__":
+	app.run("0.0.0.0", 5000)
+
